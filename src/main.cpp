8717d6d5ea097028d71efc505905b5f995ad8d81
magley <60552482+magley@users.noreply.github.com>
2022-05-08 12:02:22 +0200

    feature: Add test files

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,14 @@
 #include "SDL2/SDL_image.h"
 #include <iostream>
 #include <vector>
+#include <fstream>
 #include <map>
 #include <algorithm>
 #include "tbb/tbb.h"
 #include "tbb/parallel_for.h"
 #include "tbb/parallel_reduce.h"
 #include "tbb/tick_count.h"
+#include <cassert>
 
 #include "mathutil.h"
 #include "convex_hull.h"
@@ -18,36 +20,11 @@ using namespace tbb;
 
 #define WIN_SIZE 800
 
-vector<Vec2> generate_points() {
-	vector<Vec2> points;
-
-	for (int i = 0; i < 800; i++) {
-		Vec2 p;
-		const int margin = 75;
-		p.x = random_range(margin, WIN_SIZE - margin);
-		p.y = random_range(margin, WIN_SIZE - margin);
-		points.push_back(p);
-	}
-	return points;
-}
-
-vector<Vec2> get_hull(const vector<Vec2>& points, int cutoff) {
-	vector<Vec2> hull = convex_hull_divide_and_conquer(points, cutoff);
-	sort_by_polar_coords(hull);
-	return hull;
-}
-
-vector<Vec2> get_hull_parallel(const vector<Vec2>& points, int cutoff) {
-	vector<Vec2> hull = convex_hull_divide_and_conquer_parallel(points, cutoff);
-	sort_by_polar_coords(hull);
-	return hull;
-}
-
 struct stats_t {
-	int cutoff;
-	double time_serial;
-	double time_parallel;
-	double speedup;
+	int cutoff = 0;
+	double time_serial = 0;
+	double time_parallel = 0;
+	double speedup = 0;
 
 	stats_t() {}
 
@@ -65,9 +42,106 @@ struct stats_t {
 			<< "parallel:" << time_parallel << "\n"
 			<< "speedup: " << speedup << "\n"
 			<< "\n"
-		;
+			;
 	}
 };
+
+vector<Vec2> generate_points(string filename) {
+	ifstream f(filename);
+	vector<Vec2> points;
+	double x, y;
+	while (f >> x >> y) {
+		points.push_back(Vec2(x, y));
+	}
+
+	return points;
+}
+
+void output_points(const vector<Vec2>& points, string filename) {
+	ofstream f(filename);
+	f.clear();
+
+	for (const Vec2& v : points) {
+		f << v.x << " " << v.y << "\n";
+	}
+}
+
+vector<Vec2> generate_points(int num_of_points) {
+	vector<Vec2> points;
+
+	for (int i = 0; i < num_of_points; i++) {
+		Vec2 p;
+		double pctg = (double)i / num_of_points;
+		const int margin = 200 * pctg;
+		p.x = random_range(margin, WIN_SIZE - margin);
+		p.y = random_range(margin, WIN_SIZE - margin);
+		points.push_back(p);
+	}
+
+	return points;
+}
+
+vector<Vec2> get_hull(const vector<Vec2>& points, int cutoff) {
+	vector<Vec2> hull = convex_hull_divide_and_conquer(points, cutoff);
+	sort_by_polar_coords(hull);
+	return hull;
+}
+
+vector<Vec2> get_hull_parallel(const vector<Vec2>& points, int cutoff) {
+	vector<Vec2> hull = convex_hull_divide_and_conquer_parallel(points, cutoff);
+	sort_by_polar_coords(hull);
+	return hull;
+}
+
+vector<Vec2> initiate(const vector<Vec2>& points) {
+	vector<Vec2> hull_parallel;
+	vector<Vec2> hull;
+	vector<stats_t> stats;
+
+	for (int i = points.size() * 0.05; i < points.size() * 0.5; i += points.size() / 10) {
+		cout << "============= " << i << " ==========\n";
+
+		tick_count time_parallel_start = tick_count::now();
+		hull_parallel = get_hull_parallel(points, i);
+		tick_count time_parallel_end = tick_count::now();
+		double t_parallel = (time_parallel_end - time_parallel_start).seconds();
+		cout << "Parallel: " << t_parallel << "s\n";
+
+		tick_count time_serial_start = tick_count::now();
+		hull = get_hull(points, i);
+		tick_count time_serial_end = tick_count::now();
+		double t_serial = (time_serial_end - time_serial_start).seconds();
+		cout << "Serial:   " << t_serial << "s\n";
+
+		double speedup = t_serial / t_parallel;
+		cout << "Speedup: " << speedup << "\n";
+
+		stats.push_back(stats_t(i, t_serial, t_parallel));
+	}
+	{
+		cout << "========================\n";
+
+		sort(stats.begin(), stats.end(), [](const stats_t& s1, const stats_t& s2) {
+			return s1.time_parallel < s2.time_parallel;
+		});
+
+		stats_t best_time = stats[0];
+		cout << "best_time:\n";
+		best_time.print_me();
+
+		sort(stats.begin(), stats.end(), [](const stats_t& s1, const stats_t& s2) {
+			return s1.speedup > s2.speedup;
+		});
+
+		stats_t best_speedup = stats[0];
+		cout << "best_speedup:\n";
+		best_speedup.print_me();
+	}
+
+	assert(is_permutation(hull.cbegin(), hull.cend(), hull_parallel.cbegin()));
+
+	return hull_parallel;
+}
 
 int main(int argc, char** argv) {
 	SDL_Init(SDL_INIT_VIDEO);
@@ -92,53 +166,8 @@ int main(int argc, char** argv) {
 	int t = time(NULL);
 	srand(t);
 
-	vector<Vec2> points = generate_points();
-	vector<Vec2> hull_parallel;
-	vector<Vec2> hull;
-
-	vector<stats_t> stats;
-
-	for (int i = 10; i < 200; i += 1) {
-		cout << "============= " << i << " ==========\n";
-
-		tick_count time_parallel_start = tick_count::now();
-		hull_parallel = get_hull_parallel(points, i);
-		tick_count time_parallel_end = tick_count::now();
-		double t_parallel = (time_parallel_end - time_parallel_start).seconds();
-		cout << "Parallel: " << t_parallel << "s\n";
-
-
-		tick_count time_serial_start = tick_count::now();
-		hull = get_hull(points, i);
-		tick_count time_serial_end = tick_count::now();
-		double t_serial = (time_serial_end - time_serial_start).seconds();
-		cout << "Serial:   " << t_serial << "s\n";
-
-		double speedup = t_serial / t_parallel;
-		cout << "Speedup: " << speedup << "\n";
-
-		stats.push_back(stats_t(i, t_serial, t_parallel));
-	}
-
-	{
-		cout << "========================\n";
-
-		sort(stats.begin(), stats.end(), [](const stats_t& s1, const stats_t& s2) {
-			return s1.time_parallel < s2.time_parallel;
-		});
-
-		stats_t best_time = stats[0];
-		cout << "best_time:\n";
-		best_time.print_me();
-
-		sort(stats.begin(), stats.end(), [](const stats_t& s1, const stats_t& s2) {
-			return s1.speedup > s2.speedup;
-		});
-
-		stats_t best_speedup = stats[0];
-		cout << "best_speedup:\n";
-		best_speedup.print_me();
-	}
+	vector<Vec2> points = generate_points("res/input_file_01.txt");
+	vector<Vec2> hull = initiate(points);
 
 	while (running) {
 		while (SDL_PollEvent(&ev)) {
@@ -150,9 +179,9 @@ int main(int argc, char** argv) {
 		SDL_SetRenderDrawColor(rend, 0, 0, 0, 255);
 		SDL_RenderClear(rend);
 
-		draw_points(rend, points, { 255, 255, 255, 32 }, Vec2(4, 4));
-		draw_points(rend, hull_parallel, { 255, 255, 255, 128 }, Vec2(4, 4));
-		draw_polygon(rend, hull_parallel, { 0, 255, 255, 200 });
+		draw_points(rend, points, { 255, 255, 255, 128 }, Vec2(4, 4));
+		draw_points(rend, hull, { 255, 255, 255, 255 }, Vec2(4, 4));
+		draw_polygon(rend, hull, { 0, 255, 255, 255 });
 
 		SDL_RenderPresent(rend);
 	}
